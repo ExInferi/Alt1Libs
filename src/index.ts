6cbdd62a98189f98dbb18d0649fd665a2b970596
Ex Inferi <dev@exinferi.nl>
2024-11-02 22:51:20 +0100

    Initial upload: chatbox app added

---
+++ src/index.ts
@@ -0,0 +1,102 @@
+import * as A1 from 'alt1/base';
+import * as OCR from 'alt1/ocr';
+
+// imports for the libraries
+import animal from './libs/animal';
+import bossTimer from './libs/bosstimer';
+import buffs from './libs/buffs';
+import chatbox from './libs/chatbox';
+import dialog from './libs/dialog';
+import dropsMenu from './libs/dropsmenu';
+import target from './libs/targetmob';
+import tooltip from './libs/tooltip';
+
+// Webpack imports for dist files
+import './index.html';
+import './appconfig.json';
+import './icon.png';
+
+// Main element
+const main = document.querySelector('main') as HTMLElement;
+
+// Helper function to disable buttons when a lib is currently active
+function toggleButtons(halt: boolean): void {
+	const buttons = document.querySelectorAll('button') as NodeListOf<HTMLButtonElement>;
+
+	buttons.forEach((button) => {
+		if (!halt && button.dataset.active === 'false') {
+			button.disabled = true;
+		} else {
+			button.disabled = false;
+		}
+	});
+}
+
+// Animal reader
+const animalButton = document.querySelector('#animal') as HTMLButtonElement;
+
+animalButton.addEventListener('click', () => {
+	main.innerHTML = 'This has not been implemented yet.';
+});
+
+// Bosstimer reader
+const bosstimerButton = document.querySelector('#bosstimer') as HTMLButtonElement;
+
+bosstimerButton.addEventListener('click', () => {
+	main.innerHTML = 'This has not been implemented yet.';
+});
+
+// Buffs reader
+const buffsButton = document.querySelector('#buffs') as HTMLButtonElement;
+
+buffsButton.addEventListener('click', () => {
+	main.innerHTML = 'This has not been implemented yet.';
+});
+
+// Chatbox reader
+const chatButton = document.querySelector('#chatbox') as HTMLButtonElement;
+let chatboxHalted = true;
+
+chatButton.addEventListener('click', () => {
+	console.log('Button clicked, chatboxHalted:', chatboxHalted);
+	chatbox(chatboxHalted, 'main');
+	chatboxHalted = !chatboxHalted;
+	chatButton.dataset.active = chatboxHalted ? 'false' : 'true';
+	// Disable the button while processing
+	toggleButtons(chatboxHalted);
+	chatButton.disabled = true;
+	chatButton.querySelector('span').textContent = 'Loading';
+
+	setTimeout(() => {
+		chatButton.disabled = false;
+		chatButton.querySelector('span').textContent = chatboxHalted ? 'Chatbox' : 'Stop';
+	}, 3000);
+});
+
+// Dialog reader
+const dialogButton = document.querySelector('#dialog') as HTMLButtonElement;
+
+dialogButton.addEventListener('click', () => {
+	main.innerHTML = 'This has not been implemented yet.';
+});
+
+// Dropsmenu reader
+const dropsmenuButton = document.querySelector('#dropsmenu') as HTMLButtonElement;
+
+dropsmenuButton.addEventListener('click', () => {
+	main.innerHTML = 'This has not been implemented yet.';
+});
+
+// Target mob reader
+const targetmobButton = document.querySelector('#targetmob') as HTMLButtonElement;
+
+targetmobButton.addEventListener('click', () => {
+	main.innerHTML = 'This has not been implemented yet.';
+});
+
+// Tooltip reader
+const tooltipButton = document.querySelector('#tooltip') as HTMLButtonElement;
+
+tooltipButton.addEventListener('click', () => {
+	main.innerHTML = 'This has not been implemented yet.';
+});
---
+++ src/libs/chatbox.ts
@@ -0,0 +1,166 @@
+// import * as A1 from '../../../alt1/dist/base';
+// import ChatboxReader, { ChatLine } from '../../../alt1/dist/chatbox';
+import * as A1 from 'alt1/base';
+import ChatboxReader, { ChatLine } from 'alt1/chatbox';
+// Create a new ChatboxReader instance
+const reader = new ChatboxReader();
+/**
+ * By default, the ChatboxReader will be able to read the colors defined in the library's default colors array.
+ * In a lot of cases, you will want to use your own definition of colors like this:
+ * reader.readargs.colors = [A1.mixColor(255, 255, 255), A1.mixColor(0, 0, 0)];
+ */
+
+// Interval for looking for the chatbox every second
+function findChat(start: boolean = true, callback: () => void): void {
+	const looking = setInterval(() => {
+		// Cancel the interval if the app is not opened in alt1 or not running
+		if (!window.alt1 || !start) {
+			clearInterval(looking);
+			reader.pos = null;
+			return;
+		}
+		// Check if the chatbox position is found
+		if (reader.pos === null && start) {
+			reader.find();
+			console.log('Chatbox position not found, trying to find...');
+		} else {
+			clearInterval(looking);
+			console.log('Chatbox position found:', reader.pos);
+			callback(); // Callback to start the other functions after finding the chatbox
+		}
+	}, 1000);
+}
+
+/**
+ * The reader can find multiple chatboxes on the screen, where the first one will be main chatbox.
+ * We can access the chatbox positions and use this to create an overlay on the chatbox that was found.
+ * We can also have users specify the specific chatbox they'd like to read from.
+ */
+
+// Highlight the chatbox currently being read from
+function highlightChatbox(start: boolean = true): void {
+	// There's no use for this if there isn't a position found
+	if (!start || !reader.pos) return;
+	// Get the position and dimensions of the chatbox
+	const rect = reader.pos.mainbox.rect;
+	// The overlay functions exist within the global alt1 namespace
+	alt1.overLayRect(
+		// The color of the overlay, in this case golden yellow
+		A1.mixColor(255, 211, 63),
+		// The position and dimensions of the chatbox
+		rect.x,
+		rect.y,
+		rect.width,
+		rect.height,
+		// How long the overlay needs to be displayed in ms
+		2000,
+		// The width of the overlay in pixels
+		3,
+	);
+}
+
+// Create selection options for which chatbox to read from
+function selectChatbox(start: boolean = true, selector: string = 'body'): void {
+	// Check if the chatbox position is found
+	if (!start || !reader.pos) return;
+	// Create new selection options
+	const select = document.createElement('select');
+	select.className = 'nisdropdown';
+	select.style.position = 'fixed';
+	// Go through all found chatboxes and create an option for each
+	reader.pos.boxes.map((box, index) => {
+		console.log(box);
+		const option = document.createElement('option');
+		option.value = index.toString();
+		option.text = `Chatbox ${index + 1}`;
+		// Check if the chatbox is the main chatbox and make that the selected option
+		box.rect === reader.pos.mainbox.rect && (option.selected = true);
+		// Add the options to the select
+		select.appendChild(option);
+	});
+	// Add an event listener to the select to change the chatbox being read from
+	select.addEventListener('change', (e) => {
+		// Get the index of the selected chatbox
+		const index = parseInt((e.target as HTMLSelectElement).value);
+		// Set the chatbox to read from the selected index
+		reader.pos.mainbox = reader.pos.boxes[index];
+		// Highlight the newly selected chatbox
+		highlightChatbox();
+	});
+	// Append the select to the specified selector
+	document.querySelector(selector)?.appendChild(select);
+}
+
+// Update the page with the chat text being read
+function updatePage(chat: ChatLine[], selector: string = 'body'): void {
+	// Filter out chat lines with no text fragments
+	const filteredChat = chat.filter((line) => line.fragments.length > 0);
+	// Cancel if there's no chat text to display
+	if (!filteredChat.length) return;
+
+	// Get the all the lines of chat text from the chatbox
+	const text = filteredChat
+		.map((line) => {
+			// Map through the fragments of the chat line and give them their original color
+			return line.fragments
+				.map((frag) => {
+					const { color, text } = frag;
+					return `<span style="color: rgb(${[...color]})">${text}</span>`;
+				})
+				.join('');
+		})
+		.join('<br>');
+
+	// Create paragraph to display the chat text
+	const p = document.createElement('p');
+	p.style.margin = '0'; // Removing any default margins for better readability
+	p.innerHTML = text;
+	document.querySelector(selector).appendChild(p);
+
+	// Scroll to the last appended child
+	p.scrollIntoView({ behavior: 'smooth', block: 'end' });
+}
+
+// Interval for reading the chatbox every half-tick
+let readingInterval: ReturnType<typeof setInterval> | null = null;
+function readChat(start: boolean = true, selector?: string): void {
+	// Cancel if there's no chatbox position or the start flag is false
+	if (!start || !reader.pos) {
+		clearInterval(readingInterval);
+		if (selector) {
+			const output = document.querySelector(selector);
+			if (output) output.innerHTML = '';
+		}
+	}
+	// Read the chatbox
+	else {
+		readingInterval = setInterval(() => {
+			const chat: ChatLine[] | null = reader.read();
+			// Update the page with the read chat
+			if (chat) updatePage(chat, selector);
+		}, 300);
+	}
+}
+
+// Starter function for the chatbox reading
+function chatbox(start: boolean = true, selector?: string): void {
+	// Clear the output if a selector is provided
+	if (selector) {
+		const output = document.querySelector(selector);
+		if (output) {
+			output.innerHTML = '';
+		} else {
+			throw new Error('Selector not found');
+		}
+	}
+
+	// Go through the process of finding, highlighting, selecting, and reading the chatbox
+	findChat(start, () => {
+		highlightChatbox(start);
+		selectChatbox(start, selector);
+		readChat(start, selector);
+		console.log('Started reading chatbox.');
+	});
+}
+
+export default chatbox;
