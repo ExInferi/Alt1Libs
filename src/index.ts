0fb1c8b3a454edf23dd9d2635b06af5197ffdca6
Ex Inferi <dev@exinferi.nl>
2024-11-18 21:44:47 +0100

    feat: add info table to display detected alt1 api info

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as A1 from 'alt1/base';
 import * as OCR from 'alt1/ocr';
-import { foundPos, FoundPos } from './libs/util';
+import { foundPos, FoundPos, checkmark, timeDiff } from './libs/util';
 
 // imports for the libraries
 import animal from './libs/animal';
@@ -49,7 +49,7 @@ if (!window.alt1) {
 		// Update the found state elements
 		foundState.forEach((element, index) => {
 			const key = Object.keys(foundPos)[index] as keyof FoundPos;
-			element.dataset.found = foundPos[key] ? '✔' : '✘';
+			element.dataset.found = checkmark(foundPos[key]);
 		});
 	};
 
@@ -125,22 +125,21 @@ if (!window.alt1) {
 	const tooltipOutput = document.querySelector('#tooltip') as HTMLDivElement;
 
 	tooltipOutput.textContent = 'This has not been implemented yet.';
-}
 
-// Settings for the app
-const settingsButton = document.querySelector('#settings') as HTMLButtonElement;
-settingsButton.onclick = openSettings;
+	// Settings for the app
+	const settingsButton = document.querySelector('#settings') as HTMLButtonElement;
+	settingsButton.onclick = openSettings;
 
-function openSettings() {
-	const settingsPopup = window.open('', 'settings', 'width=100,height=200');
+	function openSettings() {
+		const settingsPopup = window.open('', 'settings', 'width=100,height=200');
 
-	if (settingsPopup) {
-		// Get the current settings
-		const header = document.querySelector('header') as HTMLElement;
-		const isHeaderVisible = header.style.display !== 'none';
+		if (settingsPopup) {
+			// Get the current settings
+			const header = document.querySelector('header') as HTMLElement;
+			const isHeaderVisible = header.style.display !== 'none';
 
-		// Write the HTML content for the settings window
-		settingsPopup.document.write(`
+			// Write the HTML content for the settings window
+			settingsPopup.document.write(`
       <!DOCTYPE html>
       <html lang="en">
       <head>
@@ -177,29 +176,112 @@ function openSettings() {
       </html>
     `);
 
-		settingsPopup.document.close();
+			settingsPopup.document.close();
+		}
 	}
-}
 
-// Add a function to window to update the settings
-(window as any).updateSettings = function (settings: { header: boolean }) {
-	const header = document.querySelector('header') as HTMLElement;
-	header.style.display = settings.header ? 'block' : 'none';
-};
+	// Add a function to window to update the settings
+	(window as any).updateSettings = function (settings: { header: boolean }) {
+		const header = document.querySelector('header') as HTMLElement;
+		header.style.display = settings.header ? 'block' : 'none';
+	};
 
-// Toggle details as name attribute is not supported in Chromium < 120
-document.addEventListener('DOMContentLoaded', () => {
-	const details = document.querySelectorAll('details[name="libs"]');
+	// Add detected information to table
+	function buildInfo() {
+		const now = Date.now();
+		const tbody = document.querySelector('#info tbody') as HTMLTableSectionElement;
+		tbody.innerHTML = '';
+		// Bools in a separate object for easier access
+		const bool = {
+			g: alt1.permissionGameState,
+			i: alt1.permissionInstalled,
+			o: alt1.permissionOverlay,
+			p: alt1.permissionPixel,
+			l: alt1.rsLinked,
+			c: alt1.permissionGameState ? alt1.currentWorld > 0 : false,
+			h: alt1.permissionGameState ? alt1.lastWorldHop > 0 : false,
+		};
 
-	details.forEach((detail) => {
-		detail.querySelector('summary')?.addEventListener('click', (e) => {
-			e.preventDefault(); // Prevent default toggle behavior
+		const info = {
+			// Version of Alt1 Toolkit
+			'Alt1 version': alt1.version,
+			// Window skin selected by user in settings
+			'Preferred theme': alt1.skinName,
+			// Capture method used by Alt1
+			'Capture method': alt1.captureMethod,
+			// Recommended interval based on capture method
+			'Recommended interval': `${alt1.captureInterval}ms`,
+			// App permissions
+			'App installed': checkmark(bool.i),
+			'GameState permission': checkmark(bool.g),
+			'Overlay permission': checkmark(bool.o),
+			'Pixel permission': checkmark(bool.p),
+			// Display how the app was opened
+			'App opened through': bool.i ? JSON.parse(alt1.openInfo).openMethod : 'App not installed',
+			// Informations about the screen(s) detected
+			'User screen(s)': `X: ${alt1.screenX}, Y: ${alt1.screenY}, Size: ${alt1.screenWidth}x${alt1.screenHeight}`,
+			// Information about the RuneScape client window
+			'RS window linked': checkmark(bool.l),
+			'RS window':
+				bool.l ?
+					`X: ${alt1.rsX}, Y: ${alt1.rsY}, Size: ${alt1.rsWidth}x${alt1.rsHeight}`
+				:	'RS not linked',
+			'RS DPI scaling': bool.l ? `${alt1.rsScaling * 100}%` : 'RS not linked',
+			'RS active': bool.g ? checkmark(alt1.rsActive) : 'GameState required',
+			'RS last active': bool.g ? timeDiff(alt1.rsLastActive) : 'GameState required',
+			'RS ping': bool.g ? `${alt1.rsPing}ms` : 'GameState required',
+			'RS FPS': bool.g ? Math.round(alt1.rsFps) : 'GameState required',
+			// Information about the world detection
+			'Current world':
+				bool.g ?
+					bool.c ?
+						`w${alt1.currentWorld}`
+					:	'Unknown'
+				:	'GameState required',
+			'Last world hop':
+				bool.g ?
+					bool.h ?
+						timeDiff(now - alt1.lastWorldHop)
+					:	'Unknown'
+				:	'GameState required',
+		};
+		// Add the information to the table
+		for (const [key, value] of Object.entries(info)) {
+			const row = document.createElement('tr');
+			const th = document.createElement('th');
+			const td = document.createElement('td');
+			th.textContent = key;
+			td.textContent = value.toString();
+			row.appendChild(th);
+			row.appendChild(td);
+			tbody.appendChild(row);
+		}
+	}
 
-			// Close all other details
-			details.forEach((d) => d !== detail && d.removeAttribute('open'));
+	// Built table on interval
+	const buildTable = setInterval(() => buildInfo(), 1000);
 
-			// Toggle current detail
-			detail.toggleAttribute('open');
+	// Cleanup on closing the app
+	window.addEventListener('beforeunload', () => {
+		if (alt1.permissionInstalled) alt1.clearBinds();
+		clearInterval(buildTable);
+	});
+
+	// Toggle details as name attribute is not supported in Chromium < 120
+	document.addEventListener('DOMContentLoaded', () => {
+		buildInfo();
+		const details = document.querySelectorAll('details[name="libs"]');
+
+		details.forEach((detail) => {
+			detail.querySelector('summary')?.addEventListener('click', (e) => {
+				e.preventDefault(); // Prevent default toggle behavior
+
+				// Close all other details
+				details.forEach((d) => d !== detail && d.removeAttribute('open'));
+
+				// Toggle current detail
+				detail.toggleAttribute('open');
+			});
 		});
 	});
-});
+}
